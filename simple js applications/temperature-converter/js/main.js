db05534a13eb4df9c8a3f333fa24e3ccbaac5f61
Brandon Merry <Brandon.l.merry@gmail.com>
2022-02-25 19:13:42 +0700

    created js and html for temp conversion

--- simple js applications/temperature-converter/js/main.js
+++ simple js applications/temperature-converter/js/main.js
@@ -1,1 +1,22 @@
 //Write your pseduo code first! 
+
+
+
+
+// create an event listener 
+document.querySelector('#convert').addEventListener('click', convert)
+
+
+
+function convert() {
+
+    // get the value from the input (use let bc value changes)
+    let temp = document.querySelector('#temp').value
+  
+    // create a function to convert the temp
+    temp =  temp * 9/5 + 32
+
+
+    // show the value to 
+    document.querySelector('#placeToPutResult').innerText =  temp
+  }
