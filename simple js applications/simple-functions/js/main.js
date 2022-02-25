5f8c34290c9808fa2e79c16e7ea5721a350257ea
Brandon Merry <Brandon.l.merry@gmail.com>
2022-02-26 00:42:23 +0700

    created functiosn with arguments and parameters

---
+++ simple js applications/simple-functions/js/main.js
@@ -0,0 +1,51 @@
+//create a function that adds two numbers and alerts the sum
+// function sum(x , y) {
+//     alert(x+y)
+// }
+
+// sum (3,4)
+
+function sum(x , y) {
+    const sum = x + y
+    alert(sum)
+}
+
+sum (3,4)
+
+//create a function that multiplys three numbers and console logs the product
+// function multiply(x , y , z) {
+//     console.log(x * y* z)
+// }
+
+// multiply(3,3,3)
+
+
+function multiply(x , y , z) {
+    const product = x * y * z 
+    console.log(product)
+}
+
+multiply(3,3,3)
+
+//create a function that divides two numbers and returns the quotient
+// function division(x , y) {
+//     alert(x/y)
+// }
+
+// call division( 1, 2)
+
+
+function divide(x , y) {
+   return x / y
+} 
+
+console.log(divide(1/2))
+
+
+// function formula 
+
+// function name (parameters){
+    
+// }
+
+// call (argument)
