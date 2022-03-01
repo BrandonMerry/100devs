b95786a591b5fc48ad9b8b9159eade971d83091a
Brandon Merry <Brandon.l.merry@gmail.com>
2022-03-02 01:21:18 +0700

    while loops

--- simple js applications/theJSWay exercises/app.js
+++ simple js applications/theJSWay exercises/app.js
@@ -24,15 +24,56 @@ while (turn !== "x") {
 }
 
 
+// ***************************** Parity
 // Check the following program that shows even numbers (divisible by 2) between 1 and 10.
 
 for (let i = 1; i <= 10; i++) {
     if (i % 2 === 0) {
       console.log(`${i} is even`);
     }
+
   }
 
 //   Improve the program so that it also shows odd numbers. 
-
+for (let i = 1; i <= 10; i++) {
+    if (i % 2 === 0) {
+      console.log(`${i} is even`);
+    }
+    else {
+        console.log(`${i} is odd`);
+    }
+  }
 
 // Improve it again to replace the initial number 1 by a number given by the user.
+const start = parseInt(prompt("enter a number"));
+
+for (let i = start; i <= start + 10; i++) {
+    if (i % 2 === 0) {
+        console.log(`${i} is even`);
+      }
+      else {
+          console.log(`${i} is odd`);
+      }
+    }
+
+    // the short way of doing it 
+    
+        const start = parseInt(prompt("enter offset"));
+        for (let i = start; i < start + 10; i++) {
+            console.log(`${i} is ${i % 2 === 0 ? "even" : "odd"}`);
+        }
+
+// ***************************** Input validation
+// Write a program that continues to ask the user for a number until the entered number is less than or equal to 100.
+
+let i = Number(prompt('Pick a number'))
+while (i >= 100) {
+    i =  prompt('Pick another number')
+}
+
+// When you are done with the above, improve the program so that the terminating number is between 50 and 100.
+
+let i = Number(prompt('Pick a number'))
+while (i >= 100 || i <= 50) {
+    i =  prompt('Pick another number')
+}
