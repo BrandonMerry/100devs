d2def86038d88ee45ba59303cef5d56c9894c9cd
Brandon Merry <Brandon.l.merry@gmail.com>
2022-03-16 10:31:01 +0000

    array lengths and size

--- class19-materials/arrays-in-it-to-win-it/js/main.js
+++ class19-materials/arrays-in-it-to-win-it/js/main.js
@@ -1,7 +1,16 @@
 //Create an array of movies with at least three movies.
 
+let movies = ['fight club', 'mission impossible', 'top gun'];
+
 //Using the array from above, store the first movie in a variable
+let topMovie = movies[0];
 
 //Get the length of the original array and store it in a new variable
 
+let arryLen = movies.length
+console.log(arryLen);
+
 //Get the last element in that array and store it in a new variable. What if your array was really large and you didn't know the last index? Would your solution still work?
+
+let lastElmt = movies[movies.length - 1];
+console.log(lastElmt);
