30337db764b0c931448ff790ee909f42ed7dab3d
Brandon Merry <Brandon.l.merry@gmail.com>
2022-03-16 10:56:23 +0000

    maths in arrays

--- class19-materials/arrays-fight-to-the-finish/js/main.js
+++ class19-materials/arrays-fight-to-the-finish/js/main.js
@@ -1,7 +1,25 @@
 //Create an array of movie titles. Loop through the array and each element to the h2.
+let movies = ['best seller', 'golden globe winner', 'wooden spoon', 'top 100'];
 
+movies.forEach((x,i) => document.querySelector('h2').innerText += movies[1])
 
 //Create an array of numbers. Loop through the array and three to each number and replace the old number.
 
+let num = [2,4,5,6,7,];
+
+// this will always grab : element, index, then array in that order 
+
+num.forEach((item, ind)=> { num[i]= item + 3
+});
 
 //Find the average of all the numbers from question three
+
+let sum = 0;
+
+num.forEach((num)=> sum += num);
+
+// or 
+
+for (let i = 0; i <num.length; i++){
+    sum += num[i]
+}
