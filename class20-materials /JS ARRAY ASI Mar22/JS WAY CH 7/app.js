35155731588f3a7a50557c4f36115d3e78e4b3f8
Brandon Merry <Brandon.l.merry@gmail.com>
2022-03-22 15:54:15 +0000

    array practice

---
+++ class20-materials /JS ARRAY ASI Mar22/JS WAY CH 7/app.js
@@ -0,0 +1,29 @@
+// const musketeers = ["Athos","Porthos","Aramis"];
+
+// for (let i =0; i< musketeers;i++){
+//     console.log(musketeers[i]);
+// };
+
+// musketeers.pop("D'Artagnan");
+
+// musketeers.forEach(musketeer => {
+// console.log(musketeer);    
+// });
+
+// musketeers.slice(3,1);
+
+// for (const musketeer of musketeers){
+//     console.log(musketeer)
+// };
+
+
+// sum the values 
+
+// const values = [3,11,7,2,9,10];
+// let sum = 0;
+
+// for (let i=0; i<values.length; i++){
+//     sum +=values[i];
+// }
+// alert(sum);
+
