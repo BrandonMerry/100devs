9ddc6bb0c7e78b965662aab8313a49cbb08c0d40
Brandon Merry <Brandon.l.merry@gmail.com>
2022-03-24 16:01:46 +0000

    js30 arrays  exercises completed

--- class21-materials/objects-zeo/js/main.js
+++ class21-materials/objects-zeo/js/main.js
@@ -1,1 +1,24 @@
 //Create a stopwatch object that has four properties and three methods
+
+let stopwatch = {}
+
+stopwatch.currentDate = "12th of September"
+stopwatch.color = "black"
+stopwatch.buttons = 4
+stopwatch.light= fales
+
+stopwatch.timer = function(time){
+    console.log(`set timer for ${time} minutes `)
+}
+ stopwatch.timer()
+
+stopwatch.dark = function(click){
+    console.log(`your turned ${click} the light`)
+}
+ stopwatch.dark(stopwatch.light)
+
+stopwatch.date = function(day){
+    console.log(`Today is the ${day} `)
+}
+ stopwatch.date(stopwatch.currentDate)
+
