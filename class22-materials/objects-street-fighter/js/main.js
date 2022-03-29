ae64412fb410a1209ba52ca574d1e7388d2bcc0a
Brandon Merry <Brandon.l.merry@gmail.com>
2022-03-29 15:18:44 +0100

    created objects with properties and methods

--- class22-materials/objects-street-fighter/js/main.js
+++ class22-materials/objects-street-fighter/js/main.js
@@ -1,1 +1,14 @@
 //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+
+// { } is literal notation
+let sagat = {}
+
+// this are properties 
+
+sagat.height = "175cm"
+sagat.reach = '70cm'
+sagat.weight = '60km' 
+sagat.record = '200W, 10L, 5D'
+sagat.uppercut= true
+sagat.roundhouse = true
+sagat.pushKick = true
