bd100b7bf12f5d7c27b084cdd775265a8701aca0
Brandon Merry <Brandon.l.merry@gmail.com>
2022-03-30 10:43:06 +0100

    new class files

--- class22-materials/objects-street-fighter/js/main.js
+++ class22-materials/objects-street-fighter/js/main.js
@@ -1,5 +1,5 @@
-//Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
 
+// this is hand coded for sagat 
 // { } is literal notation
 let sagat = {}
 
@@ -7,8 +7,37 @@ let sagat = {}
 
 sagat.height = "175cm"
 sagat.reach = '70cm'
-sagat.weight = '60km' 
+sagat.weight = '60kl' 
 sagat.record = '200W, 10L, 5D'
+
+// these are methods 
 sagat.uppercut= true
 sagat.roundhouse = true
 sagat.pushKick = true
+
+
+// other way to do this  with implied method 
+
+let sagat = {
+    height:'175cm',
+    reach:'70cm',
+    weight:'60kl'
+}
+
+
+//Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+
+function makeSagat(uppercut, roundhouse, pushKick){
+    // properties 
+    this.uppercut = uppercut
+    this.roundhouse = roundhouse
+    this.pushKick=pushKick
+    // methods 
+    this.tko = function(){
+        console.log('win by technical knock out')
+    }
+    this.winning = function(){
+        console.log("Sagat won the fight")
+    }
+}
+
---
+++ class23-materials/turbo-review/js/main.js
@@ -0,0 +1,32 @@
+// *Variables*
+// Declare a variable and assign it to a sentence as a string. Alert if the sentence is a question
+
+const str = "is the a question?"
+
+alert(str.endsWith('?'))
+
+
+
+//Declare a variable, assign it a string of multiple words, replace every "jr. dev" with "software engineer", and print it to the console
+
+let noJoke = "jr. dev"
+
+let noJoke = "software engineer"
+
+console.log(noJoke)
+
+
+// *Functions*
+// Create a function that returns rock, paper, or scissors as randomly as possible
+
+function(){
+    let p1 = [rock,paper,scissors]
+
+}
+
+
+// *Conditionals*
+//Create a function that takes in a choice (rock, paper, or scissors) and determines if they won a game of rock paper scissors against a bot using the above function
+
+//*Loops*
+//Create a function that takes an array of choices. Play the game x times where x is the number of choices in the array. Print the results of each game to the console.
