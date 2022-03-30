fd2b5c83fe6cfb8f2632b77f127306645f8a0457
Brandon Merry <Brandon.l.merry@gmail.com>
2022-03-30 11:23:15 +0100

    Rock paper scissors review

--- class23-materials/turbo-review/js/main.js
+++ class23-materials/turbo-review/js/main.js
@@ -3,30 +3,49 @@
 
 const str = "is the a question?"
 
-alert(str.endsWith('?'))
+alert(str.endsWith('?'));
 
 
 
 //Declare a variable, assign it a string of multiple words, replace every "jr. dev" with "software engineer", and print it to the console
 
-let noJoke = "jr. dev"
+let aJoke = "I want to be a jr. dev"
 
-let noJoke = "software engineer"
-
-console.log(noJoke)
-
+console.log( aJoke.replaceAll('jr. dev', 'software engineer'));
 
 // *Functions*
 // Create a function that returns rock, paper, or scissors as randomly as possible
 
-function(){
-    let p1 = [rock,paper,scissors]
-
+function childGames(){
+    let p1 = Math.random();
+    if(p1<.33){
+        return 'rock'
+    }else if( .66 < p1 ){
+        return 'paper'
+    }else{
+        return 'scissors'
+    }
 }
 
 
 // *Conditionals*
 //Create a function that takes in a choice (rock, paper, or scissors) and determines if they won a game of rock paper scissors against a bot using the above function
 
+function choice(playerChoice){
+    let botChoice = childGames()
+    if( (playerChoice === 'rock' && botChoice === 'scissors') || (playerChoice === 'scissors' && botChoice === 'paper') ||(playerChoice === 'paper' && botChoice ==='rock')){
+        alert("you're the winner!")
+    } 
+    else if ((playerChoice === botChoice)){
+        alert("Tie game")
+    }
+    else {
+        alert("You've lost")
+    }
+}
+
+choice('paper')
+
 //*Loops*
 //Create a function that takes an array of choices. Play the game x times where x is the number of choices in the array. Print the results of each game to the console.
+
