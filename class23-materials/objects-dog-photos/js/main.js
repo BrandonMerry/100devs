51fc170f88f4a2284dbcc959910981479877ae4c
Brandon Merry <Brandon.l.merry@gmail.com>
2022-03-30 13:37:59 +0100

    created first api request

--- class23-materials/objects-dog-photos/js/main.js
+++ class23-materials/objects-dog-photos/js/main.js
@@ -1,1 +1,17 @@
 //Get a dog photo from the dog.ceo api and place the photo in the DOM
+
+// fetch sends a request to the dog.ceo server
+fetch("https://dog.ceo/api/breeds/image/random")
+    .then(res => res.json()) //parse (make sure it is) response in JSON 
+    // now console.log what was requested from the server 
+    .then(data =>{
+        console.log(data.message)
+        // passes the image url into the html for img
+        document.querySelector('img').src = data.message
+    })
+    // if there is an error, console.log the error 
+    .catch(err => {
+        console.log(`error${err}`)
+    });
+
+    
