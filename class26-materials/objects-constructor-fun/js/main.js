5dd748cfbc8a2bbe0235beccd3588a8cd0ae2c7a
Brandon Merry <Brandon.l.merry@gmail.com>
2022-04-08 11:58:08 +0100

    created a business object

--- class26-materials/objects-constructor-fun/js/main.js
+++ class26-materials/objects-constructor-fun/js/main.js
@@ -1,1 +1,15 @@
 //Create a constructor with 4 properties and 3 methods
+
+function MakeBusiness(sales, employees, location, expenses){
+    this.sales = sales
+    this.employees = employees
+    this.location = location
+    this.expenses = expenses
+
+    this.profit = function(){
+        alert (`what is the ${this.sales} price`)
+    }
+    this.overhead = function(){
+        alert(`How much do your ${this.employees} earn`)
+    }
+}
