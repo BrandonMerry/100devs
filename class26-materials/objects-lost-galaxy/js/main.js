4195808ffb08bc6ddeb729ae05e50e51c18f0fdf
Brandon Merry <Brandon.l.merry@gmail.com>
2022-04-08 11:22:44 +0100

    created a pizza object

--- class26-materials/objects-lost-galaxy/js/main.js
+++ class26-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,19 @@
-//Create a dog object that has four properties and three methods
+//Create a pizza object that has four properties and three methods
+
+let pizza ={}
+
+
+// properties
+pizza.style = "wood fire"
+pizza.toppings = ['margarita']
+pizza.company = "only from Naples"
+
+// methods
+
+pizza.deliveryTime = function(){
+    console.log("we don't do it")
+}
+
+pizza.cookTime = function(){
+    console.log("depends on the hour")
+}
