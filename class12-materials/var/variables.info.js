d97fffb12d668fac9fb5c995ec30d76299acf29e
Brandon Merry <Brandon.l.merry@gmail.com>
2022-05-02 12:16:06 +0700

    variables in JS

---
+++ class12-materials/var/variables.info.js
@@ -0,0 +1,28 @@
+// Declare two variables: admin and name.
+// Assign the value "John" to name.
+// Copy the value from name to admin.
+// Show the value of admin using alert (must output “John”).
+
+let admin, name;
+name = "John";
+admin = name;
+
+alert(admin);
+
+// Create a variable with the name of our planet. How would you name such a variable?
+
+let ourPlanet = "Earth";
+
+// Create a variable to store the name of a current visitor to a website. How would you name that variable?
+
+let currentViewer = "user on page";
+
+
+// We generally use upper case for constants that are “hard-coded”. Or, in other words, when the value is known prior to execution and directly written into the code.
+
+
+
+
+const BIRTHDAY = '18.04.1982'; // make uppercase?
+
+const AGE = someCode(BIRTHDAY); // make uppercase?
