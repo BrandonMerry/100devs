c9d8d93fdcd9bd5565adf686b50a79d7fdc756bb
Brandon Merry <Brandon.l.merry@gmail.com>
2022-05-04 07:18:37 +0700

    created inheritance for transportation class

--- class31-materials/objects-child-class/js/main.js
+++ class31-materials/objects-child-class/js/main.js
@@ -1,2 +1,24 @@
 //Create an a class and extend it - Can be anything you would like it to be! 
 
+class Transportation {
+    constructor(brand){
+        this.brand = brand
+    }
+}
+
+class Car extends Transportation{
+    constructor(brand,mirrors){
+    super(brand)
+    this.mirrors = mirrors
+    }
+}
+
+class Airplane extends Transportation {
+    constructor(brand,pilots){
+        super(brand)
+        this.pilots = pilots
+    }
+}
+
+let modelS = new Car ('Tesla', 2)
+let a340 = new Airplane("AirBus", 2)
