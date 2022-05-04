ba6724fbc3f946f49162c0d83dcf7528669ab543
Brandon Merry <Brandon.l.merry@gmail.com>
2022-05-04 08:03:32 +0700

    retyped my own class / objects

--- class31-materials/complete/js/main.js
+++ class31-materials/complete/js/main.js
@@ -1,47 +1,80 @@
-class Animal{
+// my code 
+
+class Animal {
     constructor(name){
+        // this._ represents a private property - don't touch it
         this._name = name
     }
     get name(){
         return this._name
     }
     speak(){
-        console.log(`${this._name} makes a sound`)
+        console.log(`${this._name} has some sound`)
     }
 }
-class Dog extends Animal{
+
+class Dog extends Animal {
     constructor(name,breed){
         super(name)
-        this._breed = breed
+        this._breed =breed
     }
     get breed(){
         return this._breed
     }
     speak(){
+        // original method from Animal class
         super.speak()
-        console.log(`${this.name} barks`)
-    }    
-}
-class Cat extends Animal{
-    constructor(name,breed){
-        super(name)
-        this._breed = breed
+        // creates a new display because we are inside of a class 
+        console.log(`${this.name} growls`)
     }
-    get breed(){
-        return this._breed
-    }
-    speak(){
-        super.speak()
-        console.log(`${this.name} meows`)
-    }    
 }
 
-let simba = new Dog('Simba','Shepard')
-let machi = new Dog('The Machine','Pitbull')
-let salem = new Cat('Salem', 'American Shorthair')
 
-let farm = [simba,machi,salem]
 
-for( a of farm ){
-    a.speak()
-}
+// class Animal{
+//     constructor(name){
+//         this._name = name
+//     }
+//     get name(){
+//         return this._name
+//     }
+//     speak(){
+//         console.log(`${this._name} makes a sound`)
+//     }
+// }
+// class Dog extends Animal{
+//     constructor(name,breed){
+//         super(name)
+//         this._breed = breed
+//     }
+//     get breed(){
+//         return this._breed
+//     }
+//     speak(){
+//         super.speak()
+//         console.log(`${this.name} barks`)
+//     }    
+// }
+// class Cat extends Animal{
+//     constructor(name,breed){
+//         super(name)
+//         this._breed = breed
+//     }
+//     get breed(){
+//         return this._breed
+//     }
+//     speak(){
+//         super.speak()
+//         console.log(`${this.name} meows`)
+//     }    
+// }
+
+// let simba = new Dog('Simba','Shepard')
+// let machi = new Dog('The Machine','Pitbull')
+// let salem = new Cat('Salem', 'American Shorthair')
+
+// let farm = [simba,machi,salem]
+
+// for( a of farm ){
+//     a.speak()
+// }
